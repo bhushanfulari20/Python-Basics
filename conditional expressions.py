7fe254002fef5d209fe90a23203107aeb612f454
Bhushan Fulari <bhushanfulari00@gmail.com>
2026-05-16 21:43:08 +0530

    Add files via upload

---
+++ conditional expressions.py
@@ -0,0 +1,41 @@
+# Python Basic Programs by Bhushan Fulari
+
+# 1. Check whether year is leap or not
+print("\tTo check whether year is leap or not")
+year = int(input("Enter the year: "))
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print("It is a leap year\n")
+else:
+    print("It is not a leap year\n")
+
+# 2. Check the student is pass or fail
+print("\tCheck the student is pass or fail")
+marks = int(input("Enter the obtained marks: "))
+if marks >= 35:
+    print("Pass\n")
+else:
+    print("Fail\n")
+
+# 3. Check the person is eligible for vote or not
+print("\tCheck the person is eligible for vote or not")
+age = int(input("Enter the age: "))
+if age >= 18:
+    print("Applicable for vote\n")
+else:
+    print("Not applicable for vote\n")
+
+# 4. Check the temperature
+print("\tCheck the temperature")
+temp = int(input("Enter your current temperature: "))
+if temp >= 25:
+    print("Hot\n")
+else:
+    print("Cold\n")
+
+# 5. Check the number is even or odd
+print("\tCheck the number is even or odd")
+num = int(input("Enter the number: "))
+if num % 2 == 0:
+    print("The number is even\n")
+else:
+    print("The number is odd\n")
